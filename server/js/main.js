f4b9f2ad7ed1c2dd667ec45f06814893c58c2224
Ícaro Freire <icarovf@usp.br>
2021-01-20 15:47:21 -0300

    hello world

---
+++ server/js/main.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello world!</h1>');
+});
+
+app.listen(3000, () => console.log('Server has started on http://localhost:3000/'));
